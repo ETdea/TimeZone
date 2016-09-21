8f36b6ac2414e22aedf430f138b5778492d529c2
Kahou <creator.kahou@gmail.com>
2016-09-22 01:41:30 +0800

    Add WorldTimeZoneAttribute

---
+++ WorldTimeZoneAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Specifies attributes for the System.WorldTimeZone.
+    /// </summary>
+    class WorldTimeZoneAttribute : Attribute
+    {
+        private string _infoId;
+
+        /// <summary>
+        /// The time zone identifier, which corresponds to the System.TimeZoneInfo.Id property.
+        /// </summary>
+        public string InfoId { get { return _infoId; } }
+
+        public WorldTimeZoneAttribute(string infoId)
+        {
+            this._infoId = infoId; 
+        }
+    }
+}
